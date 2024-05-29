858c0012615b7ba35499709783a7d7ae5e924c23
Manuel <manuel.llopart1@gmail.com>
2024-05-29 11:37:36 -0300

    Comienzo de tpArchivos

---
+++ tareaArchivos/alumno.h
@@ -0,0 +1,13 @@
+#ifndef ALUMNO_H_INCLUDED
+#define ALUMNO_H_INCLUDED
+
+typedef struct {
+     int legajo;
+     char nombre [30];
+     char apellido [30];
+     int edad;
+     int anioCursada;
+
+} stAlumno;
+
+#endif // ALUMNO_H_INCLUDED
---
+++ tareaArchivos/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "alumno.h"
+#include <string.h>
+
+#define AR_ALUMNOS "alumnos.dat"
+
+int main()
+{
+
+    return 0;
+}
